8e9761e95e6d4e428c70ef199faecf3a52774fe8
eNJi25 <nfischer.contact@gmail.com>
2025-01-18 14:49:42 +0100

    feat: entité race crée

---
+++ src/Repository/RaceRepository.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Repository;
+
+use App\Entity\Race;
+use Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepository;
+use Doctrine\Persistence\ManagerRegistry;
+
+/**
+ * @extends ServiceEntityRepository<Race>
+ */
+class RaceRepository extends ServiceEntityRepository
+{
+    public function __construct(ManagerRegistry $registry)
+    {
+        parent::__construct($registry, Race::class);
+    }
+
+    public function save(object $entity, bool $flush = false): void
+    {
+        $this->getEntityManager()->persist($entity);
+
+        if ($flush) {
+            $this->getEntityManager()->flush();
+        }
+    }
+
+    public function remove(object $entity, bool $flush = false): void
+    {
+        $this->getEntityManager()->remove($entity);
+
+        if ($flush) {
+            $this->getEntityManager()->flush();
+        }
+    }
+}
